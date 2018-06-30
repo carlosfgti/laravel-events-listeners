f92a04fbf0d466844bda45039f19844938579022
Carlos Ferreira <conta@especializati.com.br>
2018-06-30 16:12:21 -0300

    Create structure table posts

--- database/migrations/2018_07_04_190818_create_posts_table.php
+++ database/migrations/2018_07_04_190818_create_posts_table.php
@@ -15,7 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title', 100);
+            $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
