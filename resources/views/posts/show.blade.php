fb8c2d9bccd74698342992f6fdc732d5be78f4bf
Carlos Ferreira <conta@especializati.com.br>
2018-07-04 20:09:55 -0300

    Class css textarea

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 <form action="{{ route('comments.store') }}" method="POST">
     <input type="hidden" name="id" value="{{ $post->id }}">
     <div class="form-group">
-        <textarea name="body" id="" cols="20" rows="10" placeholder="Comentário"></textarea>
+        <textarea name="body" class="form-control" cols="20" rows="10" placeholder="Comentário"></textarea>
     </div>
     <div class="form-group">
         <button type="submit" class="btn btn-default">Comentar</button>
