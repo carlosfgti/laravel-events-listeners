aeffcf25bb72a2efed3c576340a308911d5144f8
Carlos Ferreira <conta@especializati.com.br>
2018-07-04 16:32:30 -0300

    Relationships Post Comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
