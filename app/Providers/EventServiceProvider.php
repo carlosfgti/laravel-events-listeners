d101e800f2a7e6f76eec811c8ff82043b196266e
Carlos Ferreira <conta@especializati.com.br>
2018-07-05 19:34:42 -0300

    Registe Event Post Commented

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        \App\Events\CommentedPost::class => [
+            \App\Listeners\SendMailCommentedPost::class,
         ],
     ];
 
