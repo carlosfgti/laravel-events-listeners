07128cb68608516d1d3ddcb851aadf3f83b62543
Carlos Ferreira <conta@especializati.com.br>
2018-07-05 19:48:05 -0300

    Relationship user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,4 +12,9 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
