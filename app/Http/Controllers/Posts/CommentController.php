eb880e74c6c4c8cd65ff6eef9cd20fe2b394a99b
Carlos Ferreira <conta@especializati.com.br>
2018-07-05 19:35:53 -0300

    Dispatching Event CommentedPost

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Posts;
 
+use App\Events\CommentedPost;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -17,6 +18,9 @@ class CommentController extends Controller
         $data = $request->all();
         $data['user_id'] = 1; // auth()->user()->id;
         $comment = $post->comments()->create($data);
+
+        // Dispatching Event
+        event(new CommentedPost($comment));
  
         return redirect()
                     ->route('posts.show', $post->id)
