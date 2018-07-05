c4c854bd2c110a2f7235bb927d48b558100f1ec8
Carlos Ferreira <conta@especializati.com.br>
2018-07-05 09:28:58 -0300

    Create Event Commented

---
+++ app/Events/CommentedPost.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Comment;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class CommentedPost
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private $comment;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+
+    /**
+     * Comment
+     *
+     * @return \App\Models\Comment  $comment
+     */
+    public function comment(): Comment
+    {
+        return $this->comment;
+    }
+    
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
